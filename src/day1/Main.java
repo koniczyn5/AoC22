147f2f6e080971d1f1307ce434845b697069fcbf
koniczyn5 <koniczyn5@tlen.pl>
2022-12-01 09:21:32 +0100

    Day 1

--- src/day1/Main.java
+++ src/day1/Main.java
@@ -1,29 +1,46 @@
 package day1;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
 
         File file = new File("resources\\day1\\input.txt");
 
-        List<String> inputs = new ArrayList<>();
+        Map<Integer, Long> caloriesPerElf = new HashMap<>();
+        int elfNumber = 1;
 
         try {
             BufferedReader br = new BufferedReader(new FileReader(file));
             String st;
             while ((st = br.readLine()) != null) {
-                inputs.add(st);
+                if(StringUtils.isEmpty(st)) {
+                    elfNumber++;
+                } else {
+                    caloriesPerElf.merge(elfNumber, Long.valueOf(st), Long::sum);
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        System.out.println("test");
+        caloriesPerElf.entrySet().stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .limit(3)
+                .reduce((entry1, entry2) -> {
+                    entry1.setValue(entry1.getValue() + entry2.getValue());
+                    return entry1;
+                })
+                        .ifPresent(entry -> System.out.println(entry.getValue()));
+
+        System.out.println("Finished");
     }
 }
