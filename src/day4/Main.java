978ba6f602ed9c721bb2e3724bfd6dbf074944a0
koniczyn5 <koniczyn5@tlen.pl>
2022-12-04 14:25:59 +0100

    Day 4

---
+++ src/day4/Main.java
@@ -0,0 +1,77 @@
+package day4;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        File file = new File("resources\\day4\\input.txt");
+
+        int numberOfOverlappingAssignments = 0;
+        int numberOfPairs = 0;
+        int numberOfNoOverlaps = 0;
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String st;
+            while ((st = br.readLine()) != null) {
+                String[] assignmentPairs = st.split(",");
+
+                numberOfPairs++;
+                int overlapStatus = overlapStatus(assignmentPairs[0], assignmentPairs[1]);
+                numberOfOverlappingAssignments += overlapStatus == 1 ? 1 : 0;
+                numberOfNoOverlaps += overlapStatus == -1 ? 1 : 0;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        System.out.println("Part 1:");
+        System.out.println(numberOfOverlappingAssignments);
+        System.out.println("Part 2:");
+        System.out.println(numberOfPairs - numberOfNoOverlaps);
+        System.out.println("Finished");
+    }
+
+    /*
+    Positive value means full overlap,
+    Negative value means no overlap,
+    Zero means partial overlap.
+     */
+    private static int overlapStatus(String firstAssignment, String secondAssignment) {
+        List<Integer> firstSectionLimits = Arrays.stream(firstAssignment.split("-"))
+                .map(Integer::valueOf)
+                .toList();
+        List<Integer> secondSectionLimits = Arrays.stream(secondAssignment.split("-"))
+                .map(Integer::valueOf)
+                .toList();
+
+        Integer firstSectionStart = firstSectionLimits.get(0);
+        Integer firstSectionEnd = firstSectionLimits.get(1);
+        Integer secondSectionStart = secondSectionLimits.get(0);
+        Integer secondSectionEnd = secondSectionLimits.get(1);
+
+        if (firstSectionEnd < secondSectionStart
+        || secondSectionEnd < firstSectionStart) {
+            return -1;
+        }
+        if (firstSectionStart <= secondSectionStart) {
+            if (secondSectionEnd <= firstSectionEnd) {
+                return 1;
+            }
+        }
+        if (secondSectionStart <= firstSectionStart) {
+            if (firstSectionEnd <= secondSectionEnd) {
+                return 1;
+            }
+        }
+        return 0;
+    }
+}
+
